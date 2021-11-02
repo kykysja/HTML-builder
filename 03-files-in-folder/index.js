1b92a39f8af2f74519dc6f730b150b921085bb5f
kykysja <kykysja11@gmail.com>
2021-11-02 23:25:54 +0100

    feat: add solution for '03-files-in-folder' task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+  else {
+    console.log('');
+
+    files.forEach((file) => {
+      if (!file.isDirectory()) {
+        fs.stat(path.join(folderPath, `${file.name}`), (err, stats) => {
+          const fileName = path.parse(file.name).name;
+          const fileExt = path.parse(file.name).ext.slice(1);
+          const fileSizeInKb = `${stats.size / 1024}kb`;
+
+          console.log(`${fileName} - ${fileExt} - ${fileSizeInKb}`);
+        });
+      }
+    });
+  }
+});
